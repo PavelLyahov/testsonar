b048a8202a7e9d0e17c35ed38193c235777be48b
p.lyahov <p.lyahov@a1qa.com>
2023-04-26 12:51:16 +0300

    add more code

---
+++ src/main/java/com/pavel/annotations/Main.java
@@ -0,0 +1,18 @@
+package com.pavel.annotations;
+
+public class Main {
+    /**
+     * @param args
+     * bla bla bla
+     * @author Pavel
+     */
+    public static void main(String[] args) {
+        User user = new User();
+        try {
+            String nameAnnotation = User.class.getDeclaredField("name").getAnnotation(Name.class).name();
+            System.out.println(nameAnnotation);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+    }
+}
