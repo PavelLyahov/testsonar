6f5e790585bdcf7765b3a26cff0ca64abc344b0b
p.lyahov <p.lyahov@a1qa.com>
2023-05-17 22:14:46 +0300

    add another files

--- src/main/java/interfaceCheck/Action2.java
+++
@@ -1,7 +0,0 @@
-package interfaceCheck;
-
-public interface Action2 {
-    default void move(){
-        System.out.println("I action 2 move ");
-    }
-}
--- src/main/java/interfaceCheck/CheckInterface.java
+++
@@ -1,9 +0,0 @@
-package interfaceCheck;
-
-public class CheckInterface {
-    public static void main(String[] args) {
-        User pavel = new User("Pavel");
-        pavel.move();
-
-    }
-}
