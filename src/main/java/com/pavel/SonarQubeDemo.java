14825259132e12d9d6065fd4a1948646e2f53bc2
p.lyahov <p.lyahov@a1qa.com>
2023-05-19 18:48:21 +0300

    19.05 add code smells

---
+++ src/main/java/com/pavel/SonarQubeDemo.java
@@ -0,0 +1,30 @@
+package com.pavel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SonarQubeDemo {
+
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>();
+
+        for (int i = 0; i <= 10; i++) {
+            numbers.add(i);
+        }
+
+        for (int i = 0; i <= numbers.size(); i++) {
+            System.out.println("Number: " + numbers.get(i));
+        }
+
+        String message = "Hello, world!";
+        if (message.equals("Hello, world!")) {
+            System.out.println("The message is correct.");
+        } else {
+            System.out.println("The message is incorrect.");
+        }
+    }
+}
+
+
+
+
